1419d3abf59b53f577927b9ad9c959fa2a5a33ad
Steve Condylios <steve.condylios@gmail.com>
2021-10-31 05:56:22 +1100

    Exercises 9 and 10

---
+++ ex10.rb
@@ -0,0 +1,24 @@
+# Here are a few things to be mindful of when printing strings
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\nona line."
+backslash_cat = "I'm \\ a \\ cat"
+
+fat_cat = """
+I'll do a list: 
+\t* Cat food
+\t* Fish
+\t* Catnip\n\t*Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+# Handy list of escape sequences here: https://learnrubythehardway.org/book/ex10.html
+
+
+
+
+
